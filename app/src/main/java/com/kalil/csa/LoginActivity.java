cf9ab30b92027a1085b126bba204ae0ba070c341
KalilDev <kalilgamer0@gmail.com>
2019-01-28 00:53:59 -0200

    Initial commit

---
+++ app/src/main/java/com/kalil/csa/LoginActivity.java
@@ -0,0 +1,138 @@
+package com.kalil.csa;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import android.content.Intent;
+
+/**
+ * A login screen that offers login via username/password.
+ */
+public class LoginActivity extends AppCompatActivity{
+    // Log tag
+    public static final String TAG = "CSA-Shitty-App";
+
+    // UI references.
+    private EditText mUsernameView;
+    private EditText mPasswordView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set up title and layout
+        setTitle(getString(R.string.name_login));
+        setContentView(R.layout.activity_login);
+
+        // Set up the login form.
+        mUsernameView = (EditText) findViewById(R.id.username);
+        mPasswordView = (EditText) findViewById(R.id.password);
+
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button mUsernameSignInButton = (Button) findViewById(R.id.username_sign_in_button);
+        mUsernameSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+    }
+
+
+    /**
+     * Attempts to sign in the account specified by the login form.
+     * If there are form errors (invalid username, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void attemptLogin() {
+        // Reset errors.
+        mUsernameView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String username = mUsernameView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password.
+        if (TextUtils.isEmpty(password)) {
+            mPasswordView.setError(getString(R.string.error_field_required));
+            focusView = mPasswordView;
+            cancel = true;
+        } else if (!isPasswordValid(password)){
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid username.
+        if (TextUtils.isEmpty(username)) {
+            mUsernameView.setError(getString(R.string.error_field_required));
+            focusView = mUsernameView;
+            cancel = true;
+        } else if (!isUsernameValid(username)) {
+            mUsernameView.setError(getString(R.string.error_invalid_email));
+            focusView = mUsernameView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Start webview activity.
+            openWebview(username, password);
+        }
+    }
+
+
+    // Start the webview activity and parse the username and password params
+    // from the function as extra info on the intent.
+    public void openWebview(String... param) {
+        Intent webview = new Intent(this, WebviewActivity.class);
+        webview.putExtra("username", param[0]);
+        webview.putExtra("password", param[1]);
+        startActivity(webview);
+    }
+
+
+    // Function that validates the username.
+    private boolean isUsernameValid(String username) { return username.contains("CSA") && username.contains("-"); }
+
+
+    // Function that validates the password.
+    private boolean isPasswordValid(String password) { return password.length() > 6; }
+
+
+    @Override
+    public void onBackPressed() {
+        // Kill myself
+        moveTaskToBack(true);
+        android.os.Process.killProcess(android.os.Process.myPid());
+        System.exit(1);
+    }
+}
+
---
+++ app/src/main/java/com/kalil/csa/WebviewActivity.java
@@ -0,0 +1,149 @@
+package com.kalil.csa;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+public class WebviewActivity extends AppCompatActivity {
+    // Set up the Spinner.
+    private View mProgressView;
+    private View mWebView;
+    boolean loadingFinished;
+    boolean redirect;
+
+    // Set up the Webview.
+    private WebView colegio;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Gather the data from the intent to prepare
+        // the POST request in order to login
+        Bundle login = getIntent().getExtras();
+        String data = "UserName=" + login.getString("username") + "&Password=" + login.getString("password") + "&RememberMe=false";
+
+        // Set up the constant URL needed to login
+        String urlcsa = "http://mobile.csa.g12.br/EducaMobile/Account/Login";
+
+        // Set up the layout
+        setContentView(R.layout.activity_webview);
+
+        // Set up basic webview settings
+        colegio = (WebView)findViewById(R.id.webView);
+        WebSettings websettings = colegio.getSettings();
+        websettings.setJavaScriptEnabled(true);
+
+        // Set up the spinner to show up when any page is loading
+        colegio.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(
+                    WebView view, WebResourceRequest request) {
+                if (!loadingFinished) {
+                    redirect = true;
+                }
+
+                setLoading(true);
+                colegio.loadUrl(request.getUrl().toString());
+                return true;
+            }
+
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                if (!redirect) {
+                    setLoading(false);
+                }
+
+                if (loadingFinished && !redirect) {
+                    setLoading(false);
+                } else {
+                    redirect = false;
+                }
+            }
+        });
+
+        try {
+            // Try to send the POST data to login.
+            colegio.postUrl(urlcsa, data.getBytes("UTF-8"));
+            Log.i(LoginActivity.TAG, "Connection succeeded!");
+        } catch (Exception e) {
+            // Oops, there was an error, help me with an strace.
+            Log.w(LoginActivity.TAG, "Connection failed");
+            StringWriter errors = new StringWriter();
+            e.printStackTrace(new PrintWriter(errors));
+            Log.e(LoginActivity.TAG, errors.toString());
+        }
+
+        // Get the ID of the spinner and the Webview
+        // so that it can hide itself or the webview.
+        mWebView = findViewById(R.id.webView);
+        mProgressView = findViewById(R.id.progress);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if(colegio.canGoBack()) {
+            // If we can go back to a previous webpage, do it.
+            colegio.goBack();
+        } else {
+            // Else, go back to the login activity
+            openLogin();
+        }
+    }
+
+
+    // Start the login activity
+    public void openLogin() {
+        Intent login = new Intent(this, LoginActivity.class);
+        startActivity(login);
+    }
+
+
+    // This function defines the param as the new
+    // value for the loadingFinished variable, so
+    // that i can do stuff once a change is made.
+    private void setLoading(boolean newLoading){
+        if(newLoading != loadingFinished) {
+            loadingFinished = newLoading;
+            showProgress(loadingFinished);
+        }
+    }
+
+
+    // Shows the progress UI and hides the webview.
+    private void showProgress(final boolean show) {
+        int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+        mWebView.setVisibility(show ? View.GONE : View.VISIBLE);
+        mWebView.animate().setDuration(shortAnimTime).alpha(
+                show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mWebView.setVisibility(show ? View.GONE : View.VISIBLE);
+            }
+        });
+
+        mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+        mProgressView.animate().setDuration(shortAnimTime).alpha(
+                show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            }
+        });
+    }
+}
